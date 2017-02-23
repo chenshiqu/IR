602b9520b65dc6227ac252a5a0588ea5f795f198
chenshiqu <348127304@qq.com>
2017-02-22 20:00:57 -0600

    initial project, fetch robots.txt

---
+++ spider.py
@@ -0,0 +1,72 @@
+# encoding: UTF-8
+import urllib.request
+import urllib
+import re
+from collections import deque
+
+
+class Spider:
+    '''a specific crawler for http://lyle.smu.edu/~fmoore'''
+
+    def __init__(self, url, limit):
+        self.queue = deque()
+        self.visited = set()  # set of url that have been fetched
+        self.disallow = []
+        self.url = url
+        self.limit = limit
+
+    def robots(self):
+        '''fetch robots.txt and get disallow url'''
+        mark = re.compile(r'Disallow:')
+        robotsUrl = self.url + '/robots.txt'
+        urlop = urllib.request.urlopen(robotsUrl)
+        for line in urlop:
+            line = line.decode('utf-8')
+            if mark.match(line):
+                disallow = re.split(':', line)
+                disallow_url = disallow[1].strip()
+                self.disallow.append(disallow_url)
+
+    def fetch(self):
+        '''fetch url'''
+        self.queue.append(self.url)
+        cnt = 0
+
+        self.robots()  # fetch robots.txt
+
+        while self.queue:
+            url = self.queue.popleft()
+            if url in self.disallow:  # check whether the url is accessible
+                print('\ndisallow\n')
+                continue
+
+            self.visited |= {url}  # remark as visited
+
+            print('already fetch: ' + str(cnt) + '  fetching <----' + url)
+
+            cnt += 1
+            if cnt > self.limit:
+                break
+
+            urlop = urllib.request.urlopen(url)
+            if 'html' not in urlop.getheader('Content-Type'):
+                continue
+
+            # address exception
+            try:
+                data = urlop.read().decode('utf-8')
+            except:
+                continue
+
+            # fetch url from page
+            linkre = re.compile('href="(.+?)"')
+            for x in linkre.findall(data):
+                if 'http' in x and x not in self.visited:
+                    self.queue.append(x)
+                    print('add to queue---> ' + x)
+
+
+if __name__ == '__main__':
+    '''main program'''
+    spider = Spider(url='http://lyle.smu.edu/~fmoore', limit=20)
+    spider.robots()
